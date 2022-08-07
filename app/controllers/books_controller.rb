6ea94181ea921e2d0876481ca84d1a6fd87aeac5
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-08-07 13:50:48 +0000

    [Add]変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
   
+  def new
+    @book = Book.new
+  end
+  
   def index
   	@books = Book.all
-  	@book = Book.new(book_params)
+  	@book = Book.new
   end
 
   def show
@@ -10,28 +14,30 @@ class BooksController < ApplicationController
   end
 
   def create
-  	@book = Book.new(book_params)
-  	if @book.save
-  		redirect_to book_path(@book)
-  	else
-  		render :new
-  	end
+  	book = Book.new(list_params)
+    book.save
+    edirect_to book_path(book.id)  
+    if @list.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
   end
+end
 
   def edit
   	@book = Book.find(params[:id])
   end
 
   def update
-  	@book = Book.find(params[:id])
-  	@book.update(book_params)
-  	redirect_to book_path(@book)
+  	book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id) 
   end
 
   def destroy
   	@book = Book.find(params[:id])
   	@book.destroy
-  	redirect_to books_path(@book)
+  	redirect_to book_path(@book)
   end
 
   private
