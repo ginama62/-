a8c8f1df3e055aeffe2a139227a5e5d7a6a1e9ac
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-08-09 14:09:41 +0000

    [Add] edit エラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,47 +1,36 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
-  
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
-  	@books = Book.all
-  	@book = Book.new
+    @books = Book.all
   end
 
   def show
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
-
-  def create
-  	book = Book.new(list_params)
-    book.save
-    edirect_to book_path(book.id)  
-    if @list.save
-      redirect_to book_path(@book.id)
-    else
-      render :new
-  end
-end
 
   def edit
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
-  	book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id) 
+    redirect_to book_path(book.id)  
   end
-
-  def destroy
-  	@book = Book.find(params[:id])
-  	@book.destroy
-  	redirect_to book_path(@book)
-  end
-
+  
   private
+  
   def book_params
-  	params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
